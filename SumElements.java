d4b84a23a2514e9bf69287404c478ddb66fc7cf3
Parvathi <parvathiselvakumar.2006>
2026-02-21 10:25:50 +0300

    Sum of the elements

---
+++ SumElements.java
@@ -0,0 +1,13 @@
+ public class SumElements {
+    public static void main(String[] args){
+        int[] arr = {10,20,30,40,50};
+        int sum =0 ;
+        for(int i=0; i<arr.length; i++){
+            sum = sum + arr[i];//for product assign the value sum =1
+        }
+        System.out.println("Sum =" + sum);
+    }
+}
+ 
+    
+
